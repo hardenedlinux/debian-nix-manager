9e08adcfc19f516eb0c21e4aed5aa9e4234818bd
gtrunsec <gtrun@hardenedlinux.org>
2020-03-14 21:52:15 -0400

    shellHook to install Zeek with nix-shell

--- pkgs/zeek/nix-shell.nix
+++ pkgs/zeek/nix-shell.nix
@@ -1,6 +1,14 @@
 with import <nixpkgs> {};
 stdenv.mkDerivation {
-  name = "env";
+  name = "zeek";
   nativeBuildInputs = [ cmake flex bison file ];
   buildInputs = [ openssl libpcap zlib curl libmaxminddb gperftools python swig rocksdb];
+  shellHook = ''
+  wget https://old.zeek.org/downloads/zeek-3.0.3.tar.gz
+  tar -xvf zeek-3.0.3.tar.gz
+  cd zeek-3.0.3/
+  ./configure
+  make
+  sudo make install
+  '';
 }
