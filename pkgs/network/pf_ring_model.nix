90d3a88a4dec88bde1624fe29e13067da5d80aff
gtrunsec <gtrun@hardenedlinux.org>
2020-03-15 02:16:53 -0400

    nix-shell: DONE PF_RING

---
+++ pkgs/network/pf_ring_model.nix
@@ -0,0 +1,19 @@
+with import <nixpkgs> {};
+stdenv.mkDerivation {
+  name = "PF_RING";
+  nativeBuildInputs = [ cmake  ];
+  buildInputs = [ bison flex];
+  shellHook = ''
+  dir="PF_RING-7.6.0"
+  if [ -d "$dir" ]
+  then
+	echo "Skip download PF_RING src."
+  echo "if you first time running PF_RING of nix-shell
+  sudo apt-get install linux-headers-4.19.0-8-amd64"
+  else
+    wget https://github.com/ntop/PF_RING/archive/7.6.0.tar.gz
+      tar -xvf 7.6.0.tar.gz
+fi
+  cd PF_RING-7.6.0/kernel
+  '';
+}
