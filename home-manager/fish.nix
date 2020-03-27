d7aad262d250a2a23942de61eef4ad21b227cea1
gtrunsec <gtrun@hardenedlinux.org>
2020-03-26 22:45:54 -0400

    update fish time tracking

--- home-manager/fish.nix
+++ home-manager/fish.nix
@@ -54,7 +54,15 @@ with lib;
     alias .....='cd ../../../..'
     abbr -a g git
     abbr -a gr "git reset --hard"
-    abbr -a gl "git pull --rebase"   
+    abbr -a gl "git pull --rebase"
+
+    set -l project
+    if echo (pwd) | grep -qEi "^/Users/$USER/Sites/"
+        set  project (echo (pwd) | sed "s#^/Users/$USER/Sites/\\([^/]*\\).*#\\1#")
+    else
+        set  project "Terminal"
+    end
+    wakatime --write --plugin "fish-wakatime/0.0.1" --entity-type app --project "$project" --entity (echo $history[1] | cut -d ' ' -f1) 2>&1 > /dev/null&
 
     if status is-interactive
     and not set -q TMUX
