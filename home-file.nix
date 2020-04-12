2d562d4df9fea3dcfc7822fd4f0bb13d0a8d3987
GTrunsec <gtrunsec@hardenedlinux.org>
2020-04-12 00:36:00 -0700

    update emacs modules

--- home-file.nix
+++ home-file.nix
@@ -9,10 +9,28 @@ home.file = {
      rev = "f9889758694bdfe11251ac475d78a93804dbd93c";
      sha256 = "16mpc6lidmn6annyl4skdixzx7syvwdj9c5la0sidg57l8kh1rqd";
    };
+
+   
    ".direnvrc".text = ''
       source $HOME/.nix-direnv/direnvrc
     '';
 
+
+
+   ".myscript/eaf".source =pkgs.fetchFromGitHub {
+     owner = "manateelazycat";
+     repo = "emacs-application-framework";
+     rev = "320840214bcb3cd2f5a0729adbe09a0ff56c8148";
+     sha256 = "0f4wra2w78nlbfgnwlnpmn2i73lrv4kqcsnfs86asmvf3g26d5jy";
+   };
+
+
+   ".myscript/snails".source =pkgs.fetchFromGitHub {
+     owner = "manateelazycat";
+     repo = "snails";
+     rev = "7e83f3822c00ee496cce42cf69331436cb3b1379";
+     sha256 = "1448d333vny2gq4jaldl9zy62jy81ih5166l0aak3p49vv8g38bz";
+   };
   # ".emacs.d".source = pkgs.fetchFromGitHub {
   #   owner = "GTrunSec";
   #   repo = "doom-emacs";
