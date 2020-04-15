af4478b622d5be43ebd6d4123f2b004f09f9b0dd
gtrunsec <gtrun@hardenedlinux.org>
2020-04-15 04:33:18 -0400

    tmux fish to zsh

--- home-packages.nix
+++ home-packages.nix
@@ -1,34 +1,33 @@
 { config, pkgs, ... }:
 let
-  osquery = pkgs.callPackages ./pkgs/osquery { };
+    osquery = pkgs.callPackages ./pkgs/osquery { };
 in
 {
-  home.packages = with pkgs;[
-	  curl
-	  git
-    pet
-	  kitty
-	  tmux
-	  autojump
-	  cmake
-	  gcc
-	  stdenv
-    fd
-	gnumake
-    direnv
-    vim
-    gdb
-    procps
-    kmod
-#    hydra
-    postgresql
-    ripgrep
-    ag
-    fzf
-    jre_headless
-    certbot
-    aria2
-    gotty
-    nettools
-   ];
+  home.packages = with pkgs;[ curl
+	                            git
+                              pet
+	                            kitty
+	                            tmux
+	                            autojump
+	                            cmake
+	                            gcc
+	                            stdenv
+                              fd
+	                            gnumake
+                              direnv
+                              vim
+                              gdb
+                              procps
+                              kmod
+                              postgresql
+                              ripgrep
+                              ag
+                              fzf
+                              jre_headless
+                              certbot
+                              aria2
+                              gotty
+                              nettools
+                              exa
+                            ];
 }
