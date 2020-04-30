25373218c4967759cc7b96c9ce410add2d5b37c0
gtrunsec <gtrun@hardenedlinux.org>
2020-04-30 03:28:24 -0400

    Using json to install zeek plugins

--- pkgs/zeek/default.nix
+++ pkgs/zeek/default.nix
@@ -1,35 +1,54 @@
-{ pkgs ? import <nixpkgs> {} }:
+with import <nixpkgs> {};
 let
   preConfigure = (import ./shell.nix);
+  metron-bro-plugin-kafka = pkgs.fetchFromGitHub (builtins.fromJSON (builtins.readFile ./zeek-plugin.json)).metron-bro-plugin-kafka;
+  zeek-postgresql = pkgs.fetchFromGitHub (builtins.fromJSON (builtins.readFile ./zeek-plugin.json)).zeek-postgresql;
+  install_plugin = pkgs.writeScript "install_plugin" (import ./install_plugin.nix { });
 in
-pkgs.stdenv.mkDerivation rec {
+stdenv.mkDerivation rec {
   pname = "zeek";
-  version = "3.0.5";
-  
-  src = pkgs.fetchurl {
+  version = "3.0.3";
+  confdir = "/var/db/${pname}";
+  src = fetchurl {
     url = "https://old.zeek.org/downloads/zeek-${version}.tar.gz";
-    sha256 = "031q56hxg9girl9fay6kqbx7li5kfm4s30aky4s1irv2b25cl6w2";
+    sha256 = "0xlw5v83qbgy23wdcddmvan2pid28mw745g4fc1z5r18kp67i8a2";
   };
 
-  nativeBuildInputs = [ pkgs.cmake pkgs.flex pkgs.bison pkgs.file];
-  buildInputs = [ pkgs.openssl pkgs.libpcap pkgs.zlib pkgs.curl pkgs.libmaxminddb
-                  pkgs.gperftools pkgs.python pkgs.swig pkgs.rocksdb
-                  ##plugin dep
-                  pkgs.rdkafka pkgs.postgresql pkgs.nghttp2 pkgs.brotli
-                  ] ;
+  nativeBuildInputs = [ cmake flex bison file ];
+  buildInputs = [ openssl libpcap zlib curl libmaxminddb gperftools python swig rocksdb system-sendmail caf makeWrapper
+                  rdkafka postgresql
+                ];
   # Indicate where to install the python bits, since it can't put them in the "usual"
   # locations as those paths are read-only.
-  
+  ZEEK_DIST = "${placeholder "out"}";
+
+  patches = [ ./zeekctl.patch];
+
   inherit preConfigure;
 
   cmakeFlags = [
-    "-DPY_MOD_INSTALL_DIR=${placeholder "out"}/${pkgs.python.sitePackages}"
+    "-DPY_MOD_INSTALL_DIR=${placeholder "out"}/${python.sitePackages}"
     "-DENABLE_PERFTOOLS=true"
     "-DINSTALL_AUX_TOOLS=true"
+    "-DINSTALL_ZEEKCTL=true"
+    "-DZEEK_ETC_INSTALL_DIR=${placeholder "out"}/etc"
+    "-DCAF_ROOT_DIR=${caf}"
+    "-DZEEK_SPOOL_DIR=${confdir}/spool"
+    "-DZEEK_LOG_DIR=${confdir}/logs"
   ];
 
+  postFixup = ''
+        substituteInPlace $out/etc/zeekctl.cfg \
+         --replace "CfgDir = $out/etc" "CfgDir = ${confdir}/etc"
+         echo "scriptsdir = ${confdir}/scripts" >> $out/etc/zeekctl.cfg
+
+         bash ${install_plugin} metron-bro-plugin-kafka ${metron-bro-plugin-kafka} ${version}
+         bash ${install_plugin} zeek-postgresql ${zeek-postgresql} ${version}
+  '';
+
   enableParallelBuilding = true;
-  meta = with pkgs.stdenv.lib; {
+
+  meta = with stdenv.lib; {
     description = "Powerful network analysis framework much different from a typical IDS";
     homepage = https://www.bro.org/;
     license = licenses.bsd3;
