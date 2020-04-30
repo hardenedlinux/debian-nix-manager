7a543bb095a3631084a4b85c2940e8b05fe0efda
gtrunsec <gtrun@hardenedlinux.org>
2020-04-29 20:21:52 -0400

    bin shell

--- bin/deploy-home-manager.nix
+++ bin/deploy-home-manager.nix
@@ -5,6 +5,7 @@ mkdir_authdir()
 {
     sudo chown $USER /var/log
     sudo chown $USER /var/cache
+    sudo chown $USER /var/lib
     sudo chown $USER /etc
     sudo mkdir -p /var/lib/elasticsearch/
     sudo chown $USER /var/lib/elasticsearch/
@@ -22,22 +23,32 @@ mkdir_authdir()
     sudo mkdir /var/lib/kibana
     sudo chown $USER /var/lib/kibana
 
+    sudo mkdir /var/lib/hydra
+    sudo chown $USER /var/lib/hydra
+
+
 }
 
-start_servie()
+start_service()
 {
     systemctl --user start osquery.service
-    systemctl --user start elasticsearch.service
-    systemctl --user start elasticsearch.service
+    systemctl --user start elasticsearch-7x.service
+    systemctl --user start logstash.service
     systemctl --user start postgresql.service
+    systemctl --user start apache-kafka.service
+    systemctl --user start zookeeper.service
     systemctl --user start netdata.service
     systemctl --user start nix-serve.service
-    systemctl --user start  kibana..service
+    systemctl --user start kibana.service
+    systemctl --user start elasticsearch.service
 }
 
 check_service()
 {
+    systemctl --user status apache-kafka.service
+    systemctl --user status zookeeper.service
     systemctl --user status osquery.service
+    systemctl --user status elasticsearch-7x.service
     systemctl --user status elasticsearch.service
     systemctl --user status hydra-server.service
     systemctl --user status postgresql.service
@@ -55,6 +66,10 @@ case $1 in
     -c | --check) shift
                   check_service
                   ;;
+    -s | --start) shift
+                  start_service
+                  ;;
+
     * )
     shift
 esac
