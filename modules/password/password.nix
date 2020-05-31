7270504839528c47ec383426bebc3958d7526d62
gtrunsec <gtrun@hardenedlinux.org>
2020-05-31 09:48:41 +0800

    using password example

--- modules/password/password.nix
+++ modules/password/password.nix
@@ -2,6 +2,7 @@
 
 {
   options.password = with lib; {
+    ##pwgen -yB 24
     nsm-postgresql = mkOption {
       type = types.str;
       default = (builtins.fromJSON (builtins.readFile ./password.json)).nsm.psql.hydra;
