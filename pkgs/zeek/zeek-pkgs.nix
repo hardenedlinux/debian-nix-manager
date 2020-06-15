a933bdbf9887be3c50333ea6230740d35ba1947e
gtrunsec <gtrun@hardenedlinux.org>
2020-06-15 14:35:48 +0800

    update nixpkgs

--- pkgs/zeek/zeek-pkgs.nix
+++ pkgs/zeek/zeek-pkgs.nix
@@ -3,13 +3,14 @@ let
   spl-spt = pkgs.fetchFromGitHub (builtins.fromJSON (builtins.readFile ./zeek-plugin.json)).spl-spt;
   IRC-Zeek-package = pkgs.fetchFromGitHub (builtins.fromJSON (builtins.readFile ./zeek-plugin.json)).IRC-Zeek-package;
   IRC-Behavioral-Analysis = pkgs.fetchFromGitHub (builtins.fromJSON (builtins.readFile ./zeek-plugin.json)).IRC-Behavioral-Analysis;
+  zeek-EternalSafety = pkgs.fetchFromGitHub (builtins.fromJSON (builtins.readFile ./zeek-plugin.json)).zeek-EternalSafety;
 in
 {
   home.file = {
     ".zeek-script/__load__.zeek".text = ''
     @load ${spl-spt}/scripts
     ##@load ${IRC-Zeek-package}
-    @load ${IRC-Behavioral-Analysis}/zeek/irc_records.zeek
+    @load ${zeek-EternalSafety}/scripts
     '';
   };
 }
