3f6a823e9fb50906de4d670a564ff69d4c1e955b
gtrunsec <gtrunsec@hardenedlinux.org>
2020-07-11 07:48:26 +0800

    emacs: reorg agenda path and using org-roam

--- home-manager/git.nix
+++ home-manager/git.nix
@@ -3,7 +3,7 @@
   programs.git = {
     enable = true;
     userName = "gtrunsec";
-    userEmail = "gtrun@hardenedlinux.org";
+    userEmail = "gtrunsec@hardenedlinux.org";
 #    signing.key = "20C828B69E5458A0";
 #    signing.signByDefault = true;
     ignores = [ ".projectile" ".indium.json" ".ccls-cache"
