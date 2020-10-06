a4599e8da0453cf5422716154463f13ed473c966
gtrunsec <gtrunsec@hardenedlinux.org>
2020-10-06 10:31:35 +0800

    update nixpkgs

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
-    nixpkgs.url = "nixpkgs/684d5d27136f154775c95005dcce2d32943c7c9e";
+    nixpkgs.url = "nixpkgs/0cdf13328905da1d35c9395ad1de89424c511943";
     master.url = "nixpkgs/703f052de185c3dd1218165e62b105a68e05e15f";
     hardenedlinux-pkgs.url = "github:hardenedlinux/nixpkgs-hardenedlinux/master";
     emacs-overlay.url = "github:nix-community/emacs-overlay/9f0ac65857f41e134389317c6463cf2df6f05f75";
@@ -33,7 +33,7 @@
         };
           in
           {
-            devShell = import ./shell.nix { inherit pkgs; };
+            devShell = import ./shell.nix { inherit pkgs; pkgsChannel = inputs.nixpkgs;};
           }
         )
     );
