41af4533f05cb007f7a298cfce6f4bfd43bc2d50
GTrunSec <gtrunsec@hardenedlinux.org>
2021-01-17 20:50:23 -0800

    Bumpup nix

--- devShell.nix
+++ devShell.nix
@@ -11,9 +11,14 @@ pkgs.mkShell {
     home-manager
   ];
   shellHook = ''
+  export NIX_PATH="$HOME/.nix-defexpr/channels"
+  export NIX_PATH=$HOME/.nix-defexpr/channels'$'\{NIX_PATH:+:}$NIX_PATH
+  export TERM=xterm
+
   nix-channel --add https://github.com/NixOS/nixpkgs/archive/${nixpkgs-locked.rev}.tar.gz nixpkgs
   nix-channel --add https://github.com/${home-locked.owner}/${home-locked.repo}/archive/${home-locked.rev}.tar.gz home-manager
   nix-channel --update
   home-manager build -f home.nix -I nixpkgs=${pkgsChannel}
+  #home-manager build -f home.nix -I nixpkgs=${pkgsChannel} --option substituters http://221.4.35.244:8301
     '';
 }
